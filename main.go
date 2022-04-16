ca485104843cffa28e6ee74386a837918f076a0a
Mahdi Alaee <mahdi@Mahdis-MacBook-Air.local>
2022-04-16 19:14:07 +0430

    rewrite with http lib

--- main.go
+++ main.go
@@ -1,31 +1,40 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
+	"os"
 	"time"
-
-	"github.com/gin-gonic/gin"
 )
 
-type uTime struct {
-	HumanReadable string
-	UnixMilli     int64
+var UnixTime struct {
+	HumanReadable string `json:"ansic"`
+	Unixmilli     int64  `json:"unixmilli"`
+}
+var CmdArguments = os.Args
+
+func uTime(w http.ResponseWriter, r *http.Request) {
+	utime := UnixTime
+
+	utime.HumanReadable = time.Now().Format(time.ANSIC)
+	utime.Unixmilli = time.Now().UnixMilli()
+
+	json.NewEncoder(w).Encode(utime)
 }
 
-func getTime(c *gin.Context) {
-
-	var tnow = uTime{}
-
-	tnow.HumanReadable = time.Now().Format(time.ANSIC)
-	tnow.UnixMilli = time.Now().UnixMilli()
-
-	c.IndentedJSON(http.StatusOK, tnow)
-
+func handleRequests() {
+	http.HandleFunc("/", uTime)
+	if len(os.Args) <= 1 {
+		fmt.Printf("\nRunning http server on localhost:8000\n\nIf you need to run on a different ip address or port press crtl+c \nrun time-api <ip address>:<port> \n ")
+		log.Fatal(http.ListenAndServe("localhost:8000", nil))
+	} else {
+		fmt.Println("Running http server on  ", CmdArguments[1])
+		log.Fatal(http.ListenAndServe(CmdArguments[1], nil))
+	}
 }
 
 func main() {
-
-	router := gin.Default()
-	router.GET("/time", getTime)
-	router.Run("localhost:8080")
+	handleRequests()
 }
